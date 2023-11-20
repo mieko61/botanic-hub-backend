f2a1d99e194f6b639b0b28972d8318380da5ddae
Mieko <miekotominaga@gmail.com>
2023-11-19 20:50:29 -0500

    add more plantUses

--- seed-data/plantUse.js
+++ seed-data/plantUse.js
@@ -4,6 +4,61 @@ module.exports = [
     plant_id: 1,
     healthUse_id: 1,
   },
+  // {
+  //   id: 2,
+  //   plant_id: 2,
+  //   healthUse_id: 1,
+  // },
+  // {
+  //   id: 3,
+  //   plant_id: 3,
+  //   healthUse_id: 1,
+  // },
+  // {
+  //   id: 4,
+  //   plant_id: 4,
+  //   healthUse_id: 1,
+  // },
+  // {
+  //   id: 5,
+  //   plant_id: 5,
+  //   healthUse_id: 1,
+  // },
+  // {
+  //   id: 6,
+  //   plant_id: 6,
+  //   healthUse_id: 1,
+  // },
+  // {
+  //   id: 7,
+  //   plant_id: 7,
+  //   healthUse_id: 1,
+  // },
+  // {
+  //   id: 8,
+  //   plant_id: 8,
+  //   healthUse_id: 1,
+  // },
+  // {
+  //   id: 9,
+  //   plant_id: 9,
+  //   healthUse_id: 1,
+  // },
+  // {
+  //   id: 10,
+  //   plant_id: 10,
+  //   healthUse_id: 1,
+  // },
+  // {
+  //   id: 11,
+  //   plant_id: 11,
+  //   healthUse_id: 1,
+  // },
+  // {
+  //   id: 12,
+  //   plant_id: 12,
+  //   healthUse_id: 1,
+  // },
   {
     id: 2,
     plant_id: 1,
