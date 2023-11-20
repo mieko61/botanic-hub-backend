121f198786c1daaf451dd78ecd9b17b7e2ee1d73
Mieko <miekotominaga@gmail.com>
2023-11-19 21:26:18 -0500

    add categories seed

---
+++ seed-data/categories.js
@@ -0,0 +1,10 @@
+module.exports = [
+  { id: 1, name: "Digestion" },
+  { id: 2, name: "Energy Levels" },
+  { id: 3, name: "Gynecology" },
+  { id: 4, name: "Health Conditions" },
+  { id: 5, name: "Immunity" },
+  { id: 6, name: "Inflamaion" },
+  { id: 7, name: "Mental Health" },
+  { id: 8, name: "Weight Loss" },
+];
