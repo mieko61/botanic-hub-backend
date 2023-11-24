65f8f5bf62c4369ae3a8d5e8ae6b4b90d4033037
Mieko <miekotominaga@gmail.com>
2023-11-23 21:37:05 -0500

    add comment to profile-routes, still work in progress

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -9,3 +9,10 @@ router.get("/", authenticate, async (req, res) => {
   res.send(profile);
 });
 module.exports = router;
+
+// router.get("/", authenticate, async (req, res) => {
+//   const profile = await knex("user").where({ "user.id": req.user_id }).first();
+//   console.log(req.user_id);
+//   delete profile.password;
+//   res.send(profile);
+// });
